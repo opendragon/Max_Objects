114970d067fb9f21d85145db79e57b9ef3111a7f
churchoflambda <churchoflambda>
2004-03-14 23:05:35 +0000

    Import sources.

---
+++ PhidgetDevelopment/AdvancedServo/AdvancedServo.r
@@ -0,0 +1,1 @@
+#include "Carbon.r"#include "QuickDraw.r"resource 'vers' (1, "AdvancedServo") {	0x1,	0x1,	release,	0x0,	0,	"1",	"Plugin for the AdvancedServo phidget"};resource 'vers' (2, "AdvancedServo") {	0x1,	0x1,	release,	0x0,	0,	"1",	"©2003 by Schiphorst, Jaffe, Gregory and "	"Gregson."};
