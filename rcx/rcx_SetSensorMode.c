671b2e44f557f924cf89f96c74dc7d54dffdd069
churchoflambda <churchoflambda>
2005-07-25 00:16:13 +0000

    Some minor corrections.

--- rcx/rcx_SetSensorMode.c
+++ rcx/rcx_SetSensorMode.c
@@ -1,1 +1,105 @@
-/*--------------------------------------------------------------------------------------*//*                                                                                      *//*  File:       rcx_SetSensorMode.c                                                     *//*                                                                                      *//*  Contains:   The routine cmd_SetSensorMode().                                        *//*                                                                                      *//*  Written by: Norman Jaffe                                                            *//*                                                                                      *//*  Copyright:  (c) 2002 by T. H. Schiphorst, N. Jaffe, K. Gregory and G. I. Gregson.   *//*                                                                                      *//*              All rights reserved. Redistribution and use in source and binary forms, *//*              with or without modification, are permitted provided that the following *//*              conditions are met:                                                     *//*                * Redistributions of source code must retain the above copyright      *//*                  notice, this list of conditions and the following disclaimer.       *//*                * Redistributions in binary form must reproduce the above copyright   *//*                  notice, this list of conditions and the following disclaimer in the *//*                  documentation and/or other materials provided with the              *//*                  distribution.                                                       *//*                * Neither the name of the copyright holders nor the names of its      *//*                  contributors may be used to endorse or promote products derived     *//*                  from this software without specific prior written permission.       *//*                                                                                      *//*              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     *//*              "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       *//*              LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A *//*              PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      *//*              OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   *//*              SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        *//*              LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,   *//*              DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   *//*              THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     *//*              (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   *//*              OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    *//*                                                                                      *//*  Created:    2002/03/31                                                              *//*                                                                                      *//*--------------------------------------------------------------------------------------*/#include "rcx.h"/*------------------------------------ cmd_SetSensorMode ---*/Pvoid cmd_SetSensorMode  (RcxControlPtr xx,   long          slot,   PSymbol       newMode){#if (! __powerc) #pragma unused(slot, newMode)#endif /* not __powerc */  EnterCallback();  if (xx)  {#if __powerc    if (rcxSynchronize(xx))    {      if ((slot > 0) && (slot <= RCX_NUM_SENSORS))      {        uchar modeSetCommand[] = { RCX_SET_SENSOR_MODE_CMD, 0, 0 };        long  aMode = -1;        if (newMode == gRawSymbol)          aMode = RCX_SENSOR_RAW;        else if (newMode == gBooleanSymbol)          aMode = RCX_SENSOR_BOOLEAN;        else if (newMode == gTransitionSymbol)          aMode = RCX_SENSOR_TRANSITION_COUNT;        else if (newMode == gPeriodCountSymbol)          aMode = RCX_SENSOR_PERIOD_COUNTER;        else if (newMode == gPercentSymbol)          aMode = RCX_SENSOR_PERCENT_FULL_SCALE;        else if (newMode == gCelsiusSymbol)          aMode = RCX_SENSOR_CELSIUS;        else if (newMode == gFahrenheitSymbol)          aMode = RCX_SENSOR_FAHRENHEIT;        else if (newMode == gAngleStepSymbol)          aMode = RCX_SENSOR_ANGLE_STEP;        else        {          LOG_ERROR_2(OUTPUT_PREFIX "bad sensor mode (%s)", newMode->s_name)          outlet_bang(xx->fErrorBangOut);        }        if (aMode != -1)        {          modeSetCommand[1] = static_cast<uchar>(slot - 1);          modeSetCommand[2] = static_cast<uchar>(aMode);          if (rcxSendCommand(xx, modeSetCommand, sizeof(modeSetCommand),                              RCX_SET_SENSOR_MODE_REPLY, true))            outlet_bang(xx->fCommandComplete);          else            outlet_bang(xx->fErrorBangOut);        }      }      else      {        LOG_ERROR_2(OUTPUT_PREFIX "bad sensor index (%ld)", slot)        outlet_bang(xx->fErrorBangOut);      }    }    else      outlet_bang(xx->fErrorBangOut);#endif /* __powerc */  }  ExitMaxMessageHandler()} /* cmd_SetSensorMode */
+/*--------------------------------------------------------------------------------------*/
+/*                                                                                      */
+/*  File:       rcx_SetSensorMode.c                                                     */
+/*                                                                                      */
+/*  Contains:   The routine cmd_SetSensorMode().                                        */
+/*                                                                                      */
+/*  Written by: Norman Jaffe                                                            */
+/*                                                                                      */
+/*  Copyright:  (c) 2002 by T. H. Schiphorst, N. Jaffe, K. Gregory and G. I. Gregson.   */
+/*                                                                                      */
+/*              All rights reserved. Redistribution and use in source and binary forms, */
+/*              with or without modification, are permitted provided that the following */
+/*              conditions are met:                                                     */
+/*                * Redistributions of source code must retain the above copyright      */
+/*                  notice, this list of conditions and the following disclaimer.       */
+/*                * Redistributions in binary form must reproduce the above copyright   */
+/*                  notice, this list of conditions and the following disclaimer in the */
+/*                  documentation and/or other materials provided with the              */
+/*                  distribution.                                                       */
+/*                * Neither the name of the copyright holders nor the names of its      */
+/*                  contributors may be used to endorse or promote products derived     */
+/*                  from this software without specific prior written permission.       */
+/*                                                                                      */
+/*              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     */
+/*              "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       */
+/*              LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A */
+/*              PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      */
+/*              OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   */
+/*              SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        */
+/*              LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,   */
+/*              DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   */
+/*              THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     */
+/*              (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   */
+/*              OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    */
+/*                                                                                      */
+/*  Created:    2002/03/31                                                              */
+/*                                                                                      */
+/*--------------------------------------------------------------------------------------*/
+
+#include "rcx.h"
+
+/*------------------------------------ cmd_SetSensorMode ---*/
+Pvoid cmd_SetSensorMode
+  (RcxControlPtr xx,
+   long          slot,
+   PSymbol       newMode)
+{
+#if (! __powerc)
+ #pragma unused(slot, newMode)
+#endif /* not __powerc */
+  EnterCallback();
+  if (xx)
+  {
+#if __powerc
+    if (rcxSynchronize(xx))
+    {
+      if ((slot > 0) && (slot <= RCX_NUM_SENSORS))
+      {
+        uchar modeSetCommand[] = { RCX_SET_SENSOR_MODE_CMD, 0, 0 };
+        long  aMode = -1;
+
+        if (newMode == gRawSymbol)
+          aMode = RCX_SENSOR_RAW;
+        else if (newMode == gBooleanSymbol)
+          aMode = RCX_SENSOR_BOOLEAN;
+        else if (newMode == gTransitionSymbol)
+          aMode = RCX_SENSOR_TRANSITION_COUNT;
+        else if (newMode == gPeriodCountSymbol)
+          aMode = RCX_SENSOR_PERIOD_COUNTER;
+        else if (newMode == gPercentSymbol)
+          aMode = RCX_SENSOR_PERCENT_FULL_SCALE;
+        else if (newMode == gCelsiusSymbol)
+          aMode = RCX_SENSOR_CELSIUS;
+        else if (newMode == gFahrenheitSymbol)
+          aMode = RCX_SENSOR_FAHRENHEIT;
+        else if (newMode == gAngleStepSymbol)
+          aMode = RCX_SENSOR_ANGLE_STEP;
+        else
+        {
+          LOG_ERROR_2(OUTPUT_PREFIX "bad sensor mode (%s)", newMode->s_name)
+          outlet_bang(xx->fErrorBangOut);
+        }
+        if (aMode != -1)
+        {
+          modeSetCommand[1] = static_cast<uchar>(slot - 1);
+          modeSetCommand[2] = static_cast<uchar>(aMode);
+          if (rcxSendCommand(xx, modeSetCommand, sizeof(modeSetCommand),
+                              RCX_SET_SENSOR_MODE_REPLY, true))
+            outlet_bang(xx->fCommandComplete);
+          else
+            outlet_bang(xx->fErrorBangOut);
+        }
+      }
+      else
+      {
+        LOG_ERROR_2(OUTPUT_PREFIX "bad sensor index (%ld)", slot)
+        outlet_bang(xx->fErrorBangOut);
+      }
+    }
+    else
+      outlet_bang(xx->fErrorBangOut);
+#endif /* __powerc */
+  }
+  ExitMaxMessageHandler()
+} /* cmd_SetSensorMode */
--- speak/speak.h
+++ speak/speak.h
@@ -1,1 +1,146 @@
-/*--------------------------------------------------------------------------------------*//*                                                                                      *//*  File:       speak.h                                                                 *//*                                                                                      *//*  Contains:   The data type declarations and forward references to routines.          *//*                                                                                      *//*  Written by: Norman Jaffe                                                            *//*                                                                                      *//*  Copyright:  (c) 2001 by T. H. Schiphorst, N. Jaffe, K. Gregory and G. I. Gregson.   *//*                                                                                      *//*              All rights reserved. Redistribution and use in source and binary forms, *//*              with or without modification, are permitted provided that the following *//*              conditions are met:                                                     *//*                * Redistributions of source code must retain the above copyright      *//*                  notice, this list of conditions and the following disclaimer.       *//*                * Redistributions in binary form must reproduce the above copyright   *//*                  notice, this list of conditions and the following disclaimer in the *//*                  documentation and/or other materials provided with the              *//*                  distribution.                                                       *//*                * Neither the name of the copyright holders nor the names of its      *//*                  contributors may be used to endorse or promote products derived     *//*                  from this software without specific prior written permission.       *//*                                                                                      *//*              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     *//*              "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       *//*              LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A *//*              PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      *//*              OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   *//*              SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        *//*              LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,   *//*              DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   *//*              THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     *//*              (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   *//*              OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    *//*                                                                                      *//*  Created:    2001/04/03                                                              *//*                                                                                      *//*--------------------------------------------------------------------------------------*/#if (! defined(SPEAK_H_)) #define SPEAK_H_ /* */  /*#define USE_SYSLOG /* */ #include "MissingAndExtra.h" #include <Gestalt.h> #include <SpeechSynthesis.h> #include "speakBuffers.h"  #define OUR_NAME      "speak" #define OUR_RES_NUMB  17157 #define OUTPUT_PREFIX "speak: "  #define SPEAK_SAMPLE_RATE 100 /* sampling rate to determine if busy! */ #define INITIAL_VOICE 1 struct SpeakData{  Object        fObject;  SpeechChannel fSpeaker;  DataBufferHdl fBuffer;  VoiceSpec     fVoice;  POutlet       fDoneOut;  POutlet       fPausedOut;  POutlet       fResultOut;  POutlet       fErrorBangOut;  PQelem        fDoneQueue;  SpeechDoneUPP fCompletionRoutine;  short         fVoiceNumber;  bool          fActive;  bool          fPaused;  bool          fSpelling;}; /* SpeakData */typedef SpeakData * SpeakPtr;Pvoid cmd_Continue  (SpeakPtr xx);Pvoid cmd_Pause  (SpeakPtr xx);Pvoid cmd_Pitch  (SpeakPtr xx,   double   newPitch);Pvoid cmd_Pitchq  (SpeakPtr xx);Pvoid cmd_Rate  (SpeakPtr xx,   double   newRate);Pvoid cmd_Rateq  (SpeakPtr xx);Pvoid cmd_Say  (SpeakPtr xx,   PSymbol  message,   short    argc,   PAtom    argv);Pvoid cmd_Spell  (SpeakPtr xx,   PSymbol  onOff);Pvoid cmd_Stop  (SpeakPtr xx);Pvoid cmd_Voice  (SpeakPtr xx,   long     newVoice);Pvoid cmd_VoiceMax  (SpeakPtr xx);Pvoid cmd_Voiceq  (SpeakPtr xx);Pvoid cmd_Volume  (SpeakPtr xx,   double   newVolume);Pvoid cmd_Volumeq  (SpeakPtr xx);pascal void speechComplete  (SpeechChannel	chan,   long						refCon);void transmitBuffer  (SpeakPtr xx);StandardRoutineDeclarations(SpeakPtr)mextern(PSymbol) gEmptySymbol;    /* Pointer to unique Symbol for '' */mextern(PSymbol) gOffSymbol;      /* Pointer to unique Symbol for 'off' */mextern(PSymbol) gOnSymbol;       /* Pointer to unique Symbol for 'on' */mextern(PSymbol) gPitchSymbol;    /* Pointer to unique Symbol for 'pitch' */mextern(PSymbol) gRateSymbol;     /* Pointer to unique Symbol for 'rate' */mextern(PSymbol) gVoiceSymbol;    /* Pointer to unique Symbol for 'voice' */mextern(PSymbol) gVoiceMaxSymbol; /* Pointer to unique Symbol for 'voicemax' */mextern(PSymbol) gVolumeSymbol;   /* Pointer to unique Symbol for 'volume' */#endif /* not SPEAK_H_ */
+/*--------------------------------------------------------------------------------------*/
+/*                                                                                      */
+/*  File:       speak.h                                                                 */
+/*                                                                                      */
+/*  Contains:   The data type declarations and forward references to routines.          */
+/*                                                                                      */
+/*  Written by: Norman Jaffe                                                            */
+/*                                                                                      */
+/*  Copyright:  (c) 2001 by T. H. Schiphorst, N. Jaffe, K. Gregory and G. I. Gregson.   */
+/*                                                                                      */
+/*              All rights reserved. Redistribution and use in source and binary forms, */
+/*              with or without modification, are permitted provided that the following */
+/*              conditions are met:                                                     */
+/*                * Redistributions of source code must retain the above copyright      */
+/*                  notice, this list of conditions and the following disclaimer.       */
+/*                * Redistributions in binary form must reproduce the above copyright   */
+/*                  notice, this list of conditions and the following disclaimer in the */
+/*                  documentation and/or other materials provided with the              */
+/*                  distribution.                                                       */
+/*                * Neither the name of the copyright holders nor the names of its      */
+/*                  contributors may be used to endorse or promote products derived     */
+/*                  from this software without specific prior written permission.       */
+/*                                                                                      */
+/*              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     */
+/*              "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       */
+/*              LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A */
+/*              PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      */
+/*              OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   */
+/*              SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        */
+/*              LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,   */
+/*              DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   */
+/*              THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     */
+/*              (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   */
+/*              OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    */
+/*                                                                                      */
+/*  Created:    2001/04/03                                                              */
+/*                                                                                      */
+/*--------------------------------------------------------------------------------------*/
+
+#if (! defined(SPEAK_H_))
+ #define SPEAK_H_ /* */
+ 
+ /*#define USE_SYSLOG /* */
+
+ #include "MissingAndExtra.h"
+ #include <Gestalt.h>
+ #include <SpeechSynthesis.h>
+
+ #include "speakBuffers.h"
+ 
+ #define OUR_NAME      "speak"
+ #define OUR_RES_NUMB  17157
+ #define OUTPUT_PREFIX "speak: "
+ 
+ #define SPEAK_SAMPLE_RATE 100 /* sampling rate to determine if busy! */
+ #define INITIAL_VOICE 1
+ 
+struct SpeakData
+{
+  Object        fObject;
+  SpeechChannel fSpeaker;
+  DataBufferHdl fBuffer;
+  VoiceSpec     fVoice;
+  POutlet       fDoneOut;
+  POutlet       fPausedOut;
+  POutlet       fResultOut;
+  POutlet       fErrorBangOut;
+  PQelem        fDoneQueue;
+  SpeechDoneUPP fCompletionRoutine;
+  short         fVoiceNumber;
+  bool          fActive;
+  bool          fPaused;
+  bool          fSpelling;
+}; /* SpeakData */
+
+typedef SpeakData * SpeakPtr;
+
+Pvoid cmd_Continue
+  (SpeakPtr xx);
+
+Pvoid cmd_Pause
+  (SpeakPtr xx);
+
+Pvoid cmd_Pitch
+  (SpeakPtr xx,
+   double   newPitch);
+
+Pvoid cmd_Pitchq
+  (SpeakPtr xx);
+
+Pvoid cmd_Rate
+  (SpeakPtr xx,
+   double   newRate);
+
+Pvoid cmd_Rateq
+  (SpeakPtr xx);
+
+Pvoid cmd_Say
+  (SpeakPtr xx,
+   PSymbol  message,
+   short    argc,
+   PAtom    argv);
+
+Pvoid cmd_Spell
+  (SpeakPtr xx,
+   PSymbol  onOff);
+
+Pvoid cmd_Stop
+  (SpeakPtr xx);
+
+Pvoid cmd_Voice
+  (SpeakPtr xx,
+   long     newVoice);
+
+Pvoid cmd_VoiceMax
+  (SpeakPtr xx);
+
+Pvoid cmd_Voiceq
+  (SpeakPtr xx);
+
+Pvoid cmd_Volume
+  (SpeakPtr xx,
+   double   newVolume);
+
+Pvoid cmd_Volumeq
+  (SpeakPtr xx);
+
+pascal void speechComplete
+  (SpeechChannel	chan,
+   long						refCon);
+
+void transmitBuffer
+  (SpeakPtr xx);
+
+StandardRoutineDeclarations(SpeakPtr)
+
+mextern(PSymbol) gEmptySymbol;    /* Pointer to unique Symbol for '' */
+mextern(PSymbol) gOffSymbol;      /* Pointer to unique Symbol for 'off' */
+mextern(PSymbol) gOnSymbol;       /* Pointer to unique Symbol for 'on' */
+mextern(PSymbol) gPitchSymbol;    /* Pointer to unique Symbol for 'pitch' */
+mextern(PSymbol) gRateSymbol;     /* Pointer to unique Symbol for 'rate' */
+mextern(PSymbol) gVoiceSymbol;    /* Pointer to unique Symbol for 'voice' */
+mextern(PSymbol) gVoiceMaxSymbol; /* Pointer to unique Symbol for 'voicemax' */
+mextern(PSymbol) gVolumeSymbol;   /* Pointer to unique Symbol for 'volume' */
+
+#endif /* not SPEAK_H_ */
