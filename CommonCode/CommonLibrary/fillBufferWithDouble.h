8b3d2d565c391c0d1b13434d73a962783e23ea2a
Norman Jaffe <turing@shaw.ca>
2015-11-29 21:49:38 -0800

    Changed all projects to build for deployment by default, which builds for all supported architectures. Also, the %f and %g formats do not work with any of the Max formatted output -- created a new function, fillBufferWithDouble, that uses snprintf to convert a double value to a string.

---
+++ CommonCode/CommonLibrary/fillBufferWithDouble.h
@@ -0,0 +1,52 @@
+/*--------------------------------------------------------------------------------------*/
+/*                                                                                      */
+/*  File:       fillBufferWithDouble.h                                                  */
+/*                                                                                      */
+/*  Contains:   The data type declarations and forward references to routines.          */
+/*                                                                                      */
+/*  Written by: Norman Jaffe                                                            */
+/*                                                                                      */
+/*  Copyright:  (c) 2002 by T. H. Schiphorst, N. Jaffe, K. Gregory and G. I. Gregson.   */
+/*                                                                                      */
+/*              All rights reserved. Redistribution and use in source and binary forms, */
+/*              with or without modification, are permitted provided that the following */
+/*              conditions are met:                                                     */
+/*                * Redistributions of source code must retain the above copyright      */
+/*                  notice, this list of conditions and the following disclaimer.       */
+/*                * Redistributions in binary form must reproduce the above copyright   */
+/*                  notice, this list of conditions and the following disclaimer in the */
+/*                  documentation and/or other materials provided with the              */
+/*                  distribution.                                                       */
+/*                * Neither the name of the copyright holders nor the names of its      */
+/*                  contributors may be used to endorse or promote products derived     */
+/*                  from this software without specific prior written permission.       */
+/*                                                                                      */
+/*              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     */
+/*              "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       */
+/*              LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A */
+/*              PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      */
+/*              OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   */
+/*              SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        */
+/*              LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,   */
+/*              DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   */
+/*              THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     */
+/*              (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   */
+/*              OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    */
+/*                                                                                      */
+/*  Created:    2002/04/13                                                              */
+/*                                                                                      */
+/*--------------------------------------------------------------------------------------*/
+
+#if (! defined(FILLBUFFERWITHDOUBLE_H_))
+# define FILLBUFFERWITHDOUBLE_H_ /* */
+
+# include "missingAndExtra.h"
+
+# define FILLBUFFERWITHDOUBLE_HEADER \
+int fillBufferWithDouble(char *       outBuffer,\
+                         const size_t bufferSize,\
+                         const double value)
+
+FILLBUFFERWITHDOUBLE_HEADER;
+
+#endif /* not FILLBUFFERWITHDOUBLE_H_ */
